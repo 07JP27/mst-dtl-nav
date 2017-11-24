a3a07659b8978aa2857ed0b587480d6ed34bc7bc
07JP27 <jynpei.t@gmail.com>
2017-11-24 18:15:44 +0900

    アニメーションボタンの実装
    クリックすると凹むボタン

---
+++ mast-detail/Controls/ImageButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace mastdetail.Controls
+{
+    public class ImageButton: Button
+    {
+        public ImageButton(): base()
+        {
+            const int _animationTime = 100;
+            Pressed += async (sender, e) =>
+            {
+                var btn = sender as ImageButton;
+
+                if (btn != null)
+                {
+                    await btn.ScaleTo(0.5, _animationTime);
+                    await btn.ScaleTo(1, _animationTime);
+                }
+            };
+        }
+    }
+}
